5d5fce8af0b84c8b646bcc8c481cd354cc1e909e
sadanand-DGB <sadanand.y@diggibyte.com>
2026-09-01 17:39:44 +0530

    Add loops practice

---
+++ src/9_loops/9.1_while_loop.py
@@ -0,0 +1,14 @@
+#example of while loop with break statement
+i= 1  
+while i <= 10:
+    print(i)
+    if i == 5:
+        break
+    i+= 1
+
+#example of while loop with list
+list = ["apple", 110, "banana", 135,]  
+i = 0
+while i < len(list):
+    print(list[i])
+    i += 1
---
+++ src/9_loops/9.2_for_loop.py
@@ -0,0 +1,11 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+# if else statement with for loop
+for fruit in fruits:    
+    if fruit == "Mango":
+        print(fruit + " is my favorite fruit")
+    else:
+        print(fruit + " is not my favorite fruit")
